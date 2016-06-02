e1094e6ecdb7633d6d0af0fb0e58a8988b44a0e2
Conor Branagan <conor.branagan@gmail.com>
2016-06-02 14:36:31 -0400

    Config works with new devenv and continues to work on host.
    Handle different config roots and set up things as needed in bash_aliases

--- deploy.sh
+++ deploy.sh
@@ -2,41 +2,24 @@
 
 set -e
 
-# First install oh-my-zsh
-if [ ! -d ~/.oh-my-zsh ]; then
-    echo "Cloning oh-myzsh"
-    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-fi
+CONFIG_LOCATIONS="$HOME
+$HOME/dev/datadog/personal-config"
 
-# Deploy each app config file.
-for f in `ls . `
+for CONFIG_ROOT in $CONFIG_LOCATIONS
 do
-    if [[ $f == "README.md" ]] || [[ $f == "deploy.sh" ]] ; then
-        continue
-    fi
+    echo "Deploying config files to $CONFIG_ROOT"
+    # Deploy each app config file.
+    for f in `ls . `
+    do
+        if [[ $f == "README.md" ]] || [[ $f == "deploy.sh" ]] ; then
+            continue
+        fi
 
-    if [ -d $f ]; then
-        mkdir -p ~/.${f}
-        cp -f -r ${f}/* ~/.${f}
-    else
-        cp -f $f ~/.${f}
-    fi
+        if [ -d $f ]; then
+            mkdir -p $CONFIG_ROOT/.${f}
+            cp -f -r ${f}/* $CONFIG_ROOT/.${f}
+        else
+            cp -f $f $CONFIG_ROOT/.${f}
+        fi
+    done
 done
-
-# Setup vim.
-mkdir -p ~/.vim/backup ~/.vim/tmp
-if [ ! -d ~/.vim/bundle/vundle ]; then
-    echo "Cloning vundle"
-    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-fi
-vim +BundleInstall +qall
-
-# Set up git.
-git config --global core.excludesfile ~/.gitignore
-if [[ -n $1  &&  -n $2 ]] ; then
-    git config --global user.name "$1"
-    git config --global user.email "$2"
-fi
-
-
-
