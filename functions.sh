557c13d30403b56c238da1677d02058dfc208a61
Conor Branagan <conor.branagan@gmail.com>
2025-06-30 17:55:52 -0400

    Vibing

--- functions.sh
+++ functions.sh
@@ -80,3 +80,22 @@ function k() {
     kubectl "$@"
   fi
 }
+
+function ccv() {
+  local env_vars=(
+    "ENABLE_BACKGROUND_TASKS=true"
+    "FORCE_AUTO_BACKGROUND_TASKS=true"
+  )
+  
+  local claude_args=()
+  
+  if [[ "$1" == "-y" ]]; then
+    claude_args+=("--dangerously-skip-permissions")
+  elif [[ "$1" == "-r" ]]; then
+    claude_args+=("--resume")
+  elif [[ "$1" == "-ry" ]] || [[ "$1" == "-yr" ]]; then
+    claude_args+=("--resume" "--dangerously-skip-permissions")
+  fi
+  
+  env "${env_vars[@]}" claude "${claude_args[@]}"
+}
